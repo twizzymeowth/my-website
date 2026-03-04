87e74bc8e600d9c01fc45253e693a8efbc675664
twizzymeowth <ckund002@odu.edu>
2026-03-04 19:10:57 +0000

    Consolidate to single Next.js app with embedded Sanity Studio
    - Merged studio/ into root with /studio route
    - Moved web/ contents to root
    - Added devcontainer for auto-start on port 3000
    - Removed redundant folders (studio/, web/, next.js/)

---
+++ sanity.config.ts
@@ -0,0 +1,17 @@
+'use client'
+
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schema } from './sanity/schema'
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+
+export default defineConfig({
+  basePath: '/studio',
+  projectId,
+  dataset,
+  schema,
+  plugins: [structureTool(), visionTool()],
+})
